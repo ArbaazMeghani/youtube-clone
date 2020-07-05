a86f134d99bc305b92721cfe18f5180759b76c5f
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-07-05 12:35:25 -0500

    Change cursor to pinter when user hovers over video item

--- frontend/src/components/VideoItem.js
+++ frontend/src/components/VideoItem.js
@@ -4,7 +4,7 @@ import { Grid, Paper, Typography } from '@material-ui/core'
 const VideoItem = ({video, onVideoSelect}) => {
   return (
     <Grid item xs={12}>
-      <Paper onClick={() => onVideoSelect(video)} style={{display: "flex", alignItems: "center"}}>
+      <Paper onClick={() => onVideoSelect(video)} style={{display: "flex", alignItems: "center", cursor: "pointer"}}>
         <img style={{marginRight: "20px", height: "120px", widht: "120px"}} alt="thumbnail" src={video.thumbnailUrl} />
         <Typography variant="subtitle1"><b>{video.title}</b></Typography>
       </Paper>
