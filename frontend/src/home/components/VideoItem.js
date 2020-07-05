6fb0e357c2c2661fcfd58c1e9865ae60e6e20b07
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-07-05 15:59:55 -0500

    Create video item for home page

--- frontend/src/home/components/VideoItem.js
+++ frontend/src/home/components/VideoItem.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Grid, Card, Typography, CardActionArea, CardMedia, CardContent } from '@material-ui/core'
 
-const VideoItem = (video, onVideoSelect) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div>
-      video
-    </div>
+    <Grid item>
+       <Card style={{maxWidth: "250px", height: "280px"}} onClick={() => onVideoSelect(video)}>
+         <CardActionArea height="100%">
+          <CardMedia style={{height: "140px"}} image={video.thumbnailUrl} title={video.title}/>
+            <CardContent style={{height: "140px"}}>
+              <Typography variant="subtitle1">{video.title}</Typography>
+            </CardContent>
+         </CardActionArea>
+       </Card>
+    </Grid>
   )
 }
 
